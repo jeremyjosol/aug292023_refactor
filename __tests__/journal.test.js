ee47318ac0e7dc17506f5e547278b78d76355200
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-29 10:31:50 -0700

    Initialize bad fail

--- __tests__/journal.test.js
+++ __tests__/journal.test.js
@@ -1,4 +1,15 @@
 // import Constructor from './../src/javascript.js';
 
 // describe('Constructor', () => {}
+describe('Entry', () => {
+
+test('it should accurately count the amount of words in the journals body sentence', () => {
+
+const journalEntry = new Entry(title, body);
+
+expect(journalEntry.checkType()).toEqual("how many words in journals body sentence");
+
+});
+
+});
 // test() => {}
